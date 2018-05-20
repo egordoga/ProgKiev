0106d0ca37a79a14a1bf69ea6d0d7d553af6df2a
egordoga <edoga@ukr.net>
2018-05-20 14:48:13 +0300

    0520

--- src/main/java/homeWork/week6DB/orderTask/util/UtilsDB.java
+++ src/main/java/homeWork/week6DB/orderTask/util/UtilsDB.java
@@ -6,7 +6,7 @@ public class UtilsDB {
 
     private static final String URL = "jdbc:mysql://localhost:3306/orderdb";
     private static final String USER = "root";
-    private static final String PASS = "doga";
+    private static final String PASS = "root";
 
     public static Connection getConnection() {
         Connection conn = null;
